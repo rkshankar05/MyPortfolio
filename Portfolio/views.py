3ba6a7054e8f5223090bb97cf43411ef6ab83505
rkshankar05 <rkshankar.13138@gmail.com>
2025-12-13 20:49:58 +0530

    wwithout image

--- Portfolio/views.py
+++ Portfolio/views.py
@@ -3,11 +3,20 @@ from .models import About, Experience
 
 def home(request):
     about = About.objects.first()
+    
+    # Clear the profile_image if it causes issues
+    if about and about.profile_image:
+        try:
+            # Test if the file exists
+            about.profile_image.url
+        except:
+            about.profile_image = None
+    
     context = {
         'about': about,
     }
     return render(request, 'Portfolio/home.html', context)
-
+    
 def experience_list(request):
     about = About.objects.first()
     experiences = Experience.objects.all()
